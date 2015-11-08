70d702557cbc94934f0fd6ae3c1e4b060f0cda4e
negarelion <negar@negars-MacBook-Pro-2.local>
2015-11-08 19:04:30 +0330

    profile

---
+++ pages/profile.php
@@ -0,0 +1,80 @@
+<?php $path_to_root = "../" ?>
+<?php require $path_to_root.'begin.php'; ?>
+<?php include $path_to_root.'header.php'; ?>
+<script src='https://www.google.com/recaptcha/api.js?hl=fa'></script>
+<section id="one">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12">
+				<form class="form-horizontal">
+	  <div class="form-group">
+	    <label for="inputEmail3" class="col-sm-2 control-label">نام: </label>
+	    <div class="col-sm-10">
+	      <input type="email" class="form-control" id="inputEmail3" placeholder="نگار">
+	    </div>
+	  </div>
+	  <div class="form-group">
+	    <label for="inputPassword3" class="col-sm-2 control-label">نام خانوادگی: </label>
+	    <div class="col-sm-10">
+	      <input type="password" class="form-control" id="inputPassword3" placeholder="بیاتی">
+	    </div>
+	  </div>
+
+		<div class="form-group">
+			<label for="inputEmail3" class="col-sm-2 control-label">آدرس: </label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="negar.baiati@yahoo.com">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="inputEmail3" class="col-sm-2 control-label">تلفن: </label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="09107600000">
+			</div>
+		</div>
+
+		<div class="form-group">
+			<label for="inputEmail3" class="col-sm-2 control-label">رمز عبور: </label>
+			<div class="col-sm-10">
+				<input type="email" class="form-control" id="inputEmail3" placeholder="12345678">
+			</div>
+		</div>
+
+
+		<!-- Modal -->
+		<div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+		  <div class="modal-dialog" role="document">
+		    <div class="modal-content">
+		      <div class="modal-header">
+		        <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+		        <h4 class="modal-title" id="myModalLabel">Modal title</h4>
+		      </div>
+		      <div class="modal-body">
+		        عملیات با موفقیت انجام شد!
+		      </div>
+		      <div class="modal-footer">
+		        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		        <button type="button" class="btn btn-primary">Save changes</button>
+		      </div>
+		    </div>
+		  </div>
+		</div>
+
+	  <div class="form-group">
+	    <div class="col-sm-offset-2 col-sm-10">
+
+	<!-- Button trigger modal -->
+	<button type="button" class="btn btn-primary btn-lg" data-toggle="modal" data-target="#myModal">
+	  ثبت تغییرات
+	</button>
+	    </div>
+	  </div>
+	</form>
+			</div>
+
+		</div>
+	</div>
+</section>
+<?php include $path_to_root.'footer.php'; ?>
+<?php require $path_to_root.'end.php'; ?>
