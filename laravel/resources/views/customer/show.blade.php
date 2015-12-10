f8da3116825429b0adcaaa82c2ce52d6f6408022
shayan-ys <shayan.yousefian@bamilo.com>
2015-12-10 20:02:35 +0330

    basic MVC for customer module made

---
+++ laravel/resources/views/customer/show.blade.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Shayanyousefian
+ * Date: 12/10/15
+ * Time: 7:45 PM
+ */
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Customer {{ $customer->firstname }}</title>
+</head>
+<body>
+<h1>Customer {{ $customer->lastname }}</h1>
+</body>
+</html>
