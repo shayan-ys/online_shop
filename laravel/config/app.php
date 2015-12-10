82abaa405411ab54269495f7aba0b9b2e563ac82
shayan-ys <shayan.yousefian@bamilo.com>
2015-12-10 17:36:40 +0330

    creating fundamentals

--- laravel/config/app.php
+++ laravel/config/app.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'url' => 'http://localhost',
+    'url' => 'http://localhost/online_shop/laravel/',
 
     /*
     |--------------------------------------------------------------------------
